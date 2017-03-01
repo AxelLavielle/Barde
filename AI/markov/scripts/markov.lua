2778a9a59ece2f73962b197647dc717700a5aa34
miele_a <alexis.miele@epitech.eu>
2017-03-01 12:50:30 +0100

    Tâche #44 - [IA] - Développer les chaines de Markov en LUA Lien : http://redmine.arnaud-p.com/issues/44#note-2

---
+++ AI/markov/scripts/markov.lua
@@ -0,0 +1,68 @@
+local markov = {}
+
+local probScale = {}
+local probNote = {}
+
+local node = nil
+
+local function addNode(actualNode)
+  if node == nil then
+    node = actualNode
+  else
+    local tmp = node
+    while tmp.next do
+      tmp = tmp.next
+    end
+    tmp.next = actualNode
+  end
+  return actualNode.scale, actualNode.note
+end
+
+local function nextNode(actualScale, actualNote)
+  local tmpNode = {note = nil, scale = nil, next = nil}
+  local actualProbScale = probScale[actualScale]
+  local actualProbNote = probNote[actualNote]
+  local randomScale = math.random(100)
+  local randomNote = math.random(100)
+  local i = 0
+  for key, value in pairs(actualProbScale) do
+    i = i + value
+    if (i > randomScale) then
+      tmpNode.scale = key
+      break
+    end
+  end
+  i = 0
+  for key, value in pairs(actualProbNote) do
+    i = i + value
+    if (i > randomNote) then
+      tmpNode.note = key
+      break
+    end
+  end
+  return tmpNode
+end
+
+function markov.generateNote(scale, note, nbNote)
+  math.randomseed(os.time())
+  probScale = scale;
+  probNote = note;
+  actualScale = "begin"
+  actualNote = "begin"
+  local i = 0
+  while i < nbNote do
+    actualScale,actualNote = addNode(nextNode(actualScale, actualNote))
+    i = i + 1
+  end
+  return node
+end
+
+function markov.printNode(node)
+  local tmp = node
+  while tmp do
+    print("Note = ", tmp.note, " Gamme = ", tmp.scale)
+    tmp = tmp.next
+  end
+end
+
+return markov
