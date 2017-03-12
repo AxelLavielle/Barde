65ac345870a70555731f2aeee2d7227ed519f26d
aplt <arnaud.p@outlook.fr>
2017-03-12 04:22:28 +0100

    Tâche #51 - [API]  - Ajout de l'authentification MongoDB Lien : http://redmine.arnaud-p.com/issues/51#note-2

--- Website/API/config/db.js
+++ Website/API/config/db.js
@@ -1,4 +1,12 @@
-module.exports = {
+
+// TODO: Variable d'env production/dev
+/*module.exports = {
     'secret': 'barde',
     'database': 'mongodb://localhost/barde'
+};*/
+
+
+module.exports = {
+    'secret': 'barde',
+    'database': 'mongodb://' + 'barde:' + 'BbPrEnNnpxhPLB2a' + '@' + process.env.MONGO_HOST + '/barde'
 };
