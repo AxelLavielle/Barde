ab5abb0141288cebe90bc1e657fa664753932f7f
Valerian Polizzi <valerian.polizzi@epitech.eu>
2017-03-29 12:38:09 +0200

    Tâche #94 : definition Titre et Description dans le routeProvider

--- Website/www/app/app.js
+++ Website/www/app/app.js
@@ -4,25 +4,45 @@ angular.module('app', ['app.factories', 'app.directives', 'app.controllers', 'ng
     .when("/",
     {
       templateUrl   : "templates/home.tpl.htm",
-      controller    : 'mainController'
+      controller    : 'mainController',
+      meta          : {
+        title         : "Barde",
+        description   : "Barde is a software/application that generate piped music for your evenings, waiting rooms, dinners, sport sessions or work."
+      }
     })
     .when("/home",
     {
       templateUrl   : "templates/home.tpl.htm",
-      controller    : 'mainController'
+      controller    : 'mainController',
+      meta          : {
+        title         : "Barde",
+        description   : "Barde is a software/application that generate piped music for your evenings, waiting rooms, dinners, sport sessions or work."
+      }
     })
     .when("/team",
     {
-      templateUrl   : "templates/equipe.tpl.htm"
+      templateUrl   : "templates/equipe.tpl.htm",
+      meta          : {
+        title         : "Barde - Team",
+        description   : "Who are we ?"
+      }
     })
     .when("/blog",
     {
-      templateUrl   : "templates/blog.tpl.htm"
+      templateUrl   : "templates/blog.tpl.htm",
+      meta          : {
+        title         : "Barde - Blog",
+        description   : "First steps ..."
+      }
     })
     .when("/contact",
     {
       templateUrl   : "templates/contact.tpl.htm",
-      controller    : 'contactController'
+      controller    : 'contactController',
+      meta          : {
+        title         : "Barde - Contact",
+        description   : "How to reach us"
+      }
     })
     .otherwise({templateUrl: 'templates/static/404.tpl.htm'});
 
@@ -33,6 +53,11 @@ angular.module('app', ['app.factories', 'app.directives', 'app.controllers', 'ng
   $httpProvider.defaults.useXDomain = true;
   AnalyticsProvider.setAccount('UA-91271933-1');  //UU-XXXXXXX-X should be your tracking code
 
-}).run(['Analytics', function (Analytics) {
+}).run(['Analytics', function (Analytics) {}])
 
+.run(['$rootScope', '$route', function($rootScope, $route) {
+    $rootScope.$on('$routeChangeSuccess', function() {
+        document.title = $route.current.meta.title;
+        document.description = $route.current.meta.desc;
+    });
 }]);
