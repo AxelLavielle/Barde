9bce0cc0d57970afca1427ee7b0a0a2f12b53eac
Unknown <aurelie.orset@epitech.eu>
2017-04-02 20:27:41 +0200

    Tâche #96 : documentation du code pour la partie affichage graphique

--- Software/Source/GraphManager/MainComponent.h
+++ Software/Source/GraphManager/MainComponent.h
@@ -20,17 +20,46 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
+
+/*! \class MainContentComponent
+* \brief classe representant le composant principal du client graphique
+*
+*  La classe gere tous les sous composants représentant la fenêtre graphique du client
+*/
+
 class MainContentComponent   : public Component
 {
 public:
     //==============================================================================
-	MusicStyleButton *blues;
+	MusicStyleButton *blues;  /*!< bouton pour générer le blues*/
 
+	/*!
+	*  \brief Constructeur
+	*
+	*  Constructeur de la classe MainContentComponent
+	*
+	*/
 	MainContentComponent();
+
+	/*!
+	*  \brief Destructeur
+	*
+	*  Destructeur de la classe MusicStyleButton
+	*/
     ~MainContentComponent();
 
-
+	/*!
+	*  \brief Affichage
+	*
+	* Fonction appelée pour l'affichage de la fenêtre
+	*/
     void paint (Graphics&) override;
+
+	/*!
+	*  \brief Resized
+	*
+	* Fonction appelée lors du changement de taille de la fenêtre
+	*/
     void resized() override;
 
 private:
--- Software/Source/GraphManager/MusicStyleButton.h
+++ Software/Source/GraphManager/MusicStyleButton.h
@@ -14,14 +14,44 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "../Player.hh"
 
+
+/*! \class MusicStyleButton
+* \brief classe representant un bouton de style musical
+*
+*  La classe gere la création de bouton pour un style musical
+*/
 class MusicStyleButton : public Button
 {
 public:
-	int width;
-	int height;
+	int width; /*!< largeur du bouton*/
+	int height; /*!< hauteur du bouton*/
+
+	/*!
+	*  \brief Constructeur
+	*
+	*  Constructeur de la classe MusicStyleButton
+	*
+	*  \param buttonName : nom du bouton / texte à afficher
+	*  \param w : largeur du bouton
+	*  \param h : heuteur du bouton
+	*/
 	MusicStyleButton(const String &buttonName, const int w, const int h);
+	/*!
+	*  \brief Destructeur
+	*
+	*  Destructeur de la classe MusicStyleButton
+	*/
 	~MusicStyleButton();
 	
+	/*!
+	*  \brief Affichage du bouton
+	*
+	*  Fonction qui s'occupe de l'affichage du bouton en fonctions de différents évènements
+	*
+	*  \param g : graphique où le bouton sera affiché
+	*  \param isMouseOverButton : booléen correspondant à l'évènement de la souris sur le bouton
+	*  \param isButtonDown : booléen correspondant à l'enfoncement du bouton
+	*/
 	void paintButton(Graphics& g, const bool isMouseOverButton, const bool isButtonDown);
 };
 
