7a87cc272f5a3d1a5e0f138e2c6c37e0304de4bf
Unknown <aurelie.orset@epitech.eu>
2017-05-28 19:20:07 +0200

    BAR-73 : Settings Utilisateur
    Creation de la class UserSettings avec les getters et setters correspondants + Doxygen.

---
+++ Software/Source/UserSettings.cpp
@@ -0,0 +1,84 @@
+/*
+  ==============================================================================
+
+    UserSettings.cpp
+    Created: 28 May 2017 6:10:55pm
+    Author:  Oreo
+
+  ==============================================================================
+*/
+
+#include "UserSettings.h"
+
+UserSettings::UserSettings()
+{
+	this->_mute = false;
+	this->_language = EN;
+	this->_volume = 50;
+	this->_BPM = 120; 
+}
+
+UserSettings::UserSettings(bool mute, Language language, int volume, int BPM)
+{
+	_mute = mute;
+	_language = language;
+	_volume = volume;
+	_BPM = BPM;
+}
+
+UserSettings::UserSettings(int volume, int BPM)
+{
+	_mute = false;
+	_language = EN;
+	_volume = volume;
+	_BPM = BPM;
+}
+
+UserSettings::~UserSettings()
+{
+
+}
+
+int UserSettings::getVolume()
+{
+	return (_volume);
+}
+
+int UserSettings::getBPM()
+{
+	return (_BPM);
+}
+
+bool UserSettings::isMute()
+{
+	return (_mute);
+}
+
+Language UserSettings::getLanguage()
+{
+	return (_language);
+}
+
+void UserSettings::setVolume(int volume)
+{
+	if (volume >= 0 && volume <= 100)
+	{
+		_volume = volume;
+	}
+}
+
+void UserSettings::setBPM(int BPM)
+{
+	_BPM = BPM;
+}
+
+void UserSettings::setMute(bool mute)
+{
+	_mute = mute;
+}
+
+void UserSettings::setLanguage(Language language)
+{
+	_language = language;
+}
+
