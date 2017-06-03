1dd4b4509c536cb59bcd2e284cc825983d11f107
vogelw_a <vogelw_a@epitech.eu>
2017-06-03 11:21:31 +0200

    BAR-96 : Documentation doxygen de Intrument.hh et Player.hh

--- Software/Source/Instrument.hh
+++ Software/Source/Instrument.hh
@@ -11,16 +11,26 @@
 #ifndef INSTRUMENT_HH_INCLUDED
 #define INSTRUMENT_HH_INCLUDED
 
+/*!
+ * \file Instrument.hh
+ * \brief File containing the Instrument class
+ */
+
 #include <string>
 #include "MidiManager/Instruments.hh"
 
+/*! \class Instrument
+ * \brief Instrument class contain instrument data
+ *
+ *  Instrument class contain instrument data needed for the sound library
+ */
 class Instrument
 {
 public:
-	std::string		name;
-	NbInstrument	nb;
-	int				channel;
-	float			velocity;
+	std::string		name;  /*!< Name of the instrument */
+	NbInstrument	nb;  /*!< Instrument number */
+	int				channel;  /*!< Channel corresponding to the instrument */
+	float			velocity;  /*!< Default velocity of the instrument */
 };
 
 #endif  // INSTRUMENT_HH_INCLUDED
--- Software/Source/Player.hh
+++ Software/Source/Player.hh
@@ -11,18 +11,47 @@
 #ifndef PLAYER_HH_INCLUDED
 #define PLAYER_HH_INCLUDED
 
+/*!
+ * \file Player.hh
+ * \brief File containing the Player class
+ */
+
 #include "AI/MusicGenerator.hh"
 #include "SoundManager/JuceSound.hh"
 
+/*! \class SoundManager
+ * \brief Player manage the playing of music provided by the IA
+ *
+ *  Player manage the communication between the IA and the sound library in order to play music
+ */
 class Player
 {
 public:
-	Player();
-	~Player();
-	void Play();
+	/*!
+ *  \brief Constructor
+ *
+ *  Create Sound Manager pointer
+ *
+ */
+Player();
+/*!
+ *  \brief Destructor
+ *
+ *  Destruct Sound Manager pointer
+ *
+ */
+~Player();
+/*!
+*  \brief Start the playing of music
+*
+*   Start the playing of music provided by the IA
+*
+*  \return play music
+*/
+void Play();
 
 private:
-	ISoundManager		*_soundManager;
+	ISoundManager		*_soundManager; /*!< Pointer to the sound manager*/
 };
 
 #endif  // PLAYER_HH_INCLUDED
