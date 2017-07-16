ca1d3dee744c6761e64b0795356fd6cac78a52d2
Aurelie Orset <aurelie.orset@epitech.eu>
2017-07-16 10:06:36 +0200

    BAR-74: UserSettings + Theme classes et documentation

---
+++ Software/Source/GraphManager/Theme.cpp
@@ -0,0 +1,75 @@
+/*
+  ==============================================================================
+
+    Theme.cpp
+    Created: 15 Jul 2017 2:22:21pm
+    Author:  Oreo
+
+  ==============================================================================
+*/
+
+#include "Theme.h"
+
+
+
+Theme::Theme()
+{
+	_buttonColor = 0;
+	_backgroundColor = 0;
+	_fontColor = 0;
+	_name = NULL;
+}
+
+Theme::Theme(int buttonColor, int backgroundColor, int fontColor, std::string name)
+{
+	_buttonColor = buttonColor;
+	_backgroundColor = backgroundColor;
+	_fontColor = fontColor;
+	_name = name;
+}
+
+
+Theme::~Theme()
+{
+
+}
+
+int Theme::getButtonColor()
+{
+	return (_buttonColor);
+}
+
+int Theme::getBackgroundColor()
+{
+	return (_backgroundColor);
+}
+
+int Theme::getFontColor()
+{
+	return (_fontColor);
+}
+
+std::string Theme::getName()
+{
+	return (_name);
+}
+
+void Theme::setButtonColor(int color)
+{
+	_buttonColor = color;
+}
+
+void Theme::setBackgroundColor(int color)
+{
+	_backgroundColor = color;
+}
+
+void Theme::setFontColor(int color)
+{
+	_fontColor = color;
+}
+
+void Theme::setName(std::string name)
+{
+	_name = name;
+}
---
+++ Software/Source/GraphManager/Theme.h
@@ -0,0 +1,152 @@
+/*
+  ==============================================================================
+
+    Theme.h
+    Created: 15 Jul 2017 2:22:21pm
+    Author:  Oreo
+
+  ==============================================================================
+*/
+
+#ifndef THEME_H_INCLUDED
+#define THEME_H_INCLUDED
+
+#include <iostream>
+#include <string>
+
+
+
+/*! \class Theme
+* \brief class used for the themes of the software
+*
+*  Used to define the different aspects of the software like:
+*  - BackgroundColor
+*  - ButtonColor
+*  - FontColor
+*/
+class Theme
+{
+public:
+
+
+
+	/*!
+	*  \brief Constructor
+	*
+	*  Default Constructor
+	*
+	*/
+	Theme();
+
+	/*!
+	*  \brief Constructor
+	*
+	*  Complete Constructor
+	*
+	*  \param buttonColor : color of the buttons.
+	*  \param backgroundColor : color of the background.
+	*  \param fontColor : color of the font.
+	*  \param name : name of the theme.
+	*/
+	Theme(int buttonColor, int backgroundColor, int fontColor, std::string name);
+
+	/*!
+	*  \brief Destructor
+	*
+	*  Theme Destructor
+	*
+	*/
+	~Theme();
+
+	/*!
+	*  \brief button color getter
+	*
+	*  gets the color of the buttons of the theme
+	*
+	*  \return the button color
+	*/
+	int getButtonColor();
+
+	/*!
+	*  \brief background color getter
+	*
+	*  gets the backgroudn color of the theme
+	*
+	*  \return the background color
+	*/
+	int getBackgroundColor();
+
+	/*!
+	*  \brief font color getter
+	*
+	*  gets the font color of the theme
+	*
+	*  \return the font color
+	*/
+	int getFontColor();
+
+	/*!
+	*  \brief name getter
+	*
+	*  gets the name of the theme
+	*
+	*  \return the name
+	*/
+	std::string getName();
+
+	/*!
+	*  \brief button color setter
+	*
+	*  sets the button color
+	*
+	*  \param color : color of the button
+	*/
+	void setButtonColor(int color);
+
+	/*!
+	*  \brief background color setter
+	*
+	*  sets the background color
+	*
+	*  \param color : color of the background
+	*/
+	void setBackgroundColor(int color);
+
+	/*!
+	*  \brief font color setter
+	*
+	*  sets the font color
+	*
+	*  \param color : color of the font
+	*/
+	void setFontColor(int color);
+
+	/*!
+	*  \brief name setter
+	*
+	*  sets the name of the theme
+	*
+	*  \param name : name of the theme
+	*/
+	void setName(std::string name);
+
+private:
+	int _buttonColor; /*!< color of all buttons in the software */
+	int _backgroundColor; /*!< background color of the software */
+	int _fontColor; /*!< fontcolor of the software */
+	std::string _name; /*!< name of the theme */
+
+};
+
+/*!
+*  \brief creates Theme from file
+*
+*  creates a Theme with the theme settings located in the settings file.
+*
+*  \param filename : name of the file where the theme settings are defined
+*  
+*  \return : Theme with the informaton of the file
+*/
+Theme parseTheme(std::string fileName);
+
+#endif  // THEME_H_INCLUDED
