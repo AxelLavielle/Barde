1151319d660735ffd9184fe05c715adaa2349415
vogelw_a <vogelw_a@epitech.eu>
2017-10-10 01:03:42 +0200

    Modif synthetiseur en cour

--- Software/Source/SoundManager/JuceSound.hh
+++ Software/Source/SoundManager/JuceSound.hh
@@ -41,6 +41,11 @@ struct SineWaveVoice  : public SynthesiserVoice
 {
     SineWaveVoice()   : currentAngle (0), angleDelta (0), level (0), tailOff (0)
     {
+      _amplitude = 0.5;
+      _frequency = 500;
+      _phase = 0.0;
+      _time = 0.0;
+      _deltaTime = 1 / getSampleRate();
     }
 
     bool canPlaySound (SynthesiserSound* sound) override
@@ -51,34 +56,34 @@ struct SineWaveVoice  : public SynthesiserVoice
     void startNote (int midiNoteNumber, float velocity,
                     SynthesiserSound*, int /*currentPitchWheelPosition*/) override
     {
-        currentAngle = 0.0;
-        level = velocity * 0.15;
-        tailOff = 0.0;
-
-        double cyclesPerSecond = MidiMessage::getMidiNoteInHertz (midiNoteNumber);
-        double cyclesPerSample = cyclesPerSecond / getSampleRate();
-
-        angleDelta = cyclesPerSample * 2.0 * double_Pi;
+    //     currentAngle = 0.0;
+    //     level = velocity * 0.15;
+    //     tailOff = 0.0;
+    //
+    //     double cyclesPerSecond = MidiMessage::getMidiNoteInHertz (midiNoteNumber);
+    //     double cyclesPerSample = cyclesPerSecond / getSampleRate();
+    //
+    //     angleDelta = cyclesPerSample * 2.0 * double_Pi;
     }
 
     void stopNote (float /*velocity*/, bool allowTailOff) override
     {
-        if (allowTailOff)
-        {
-            // start a tail-off by setting this flag. The render callback will pick up on
-            // this and do a fade out, calling clearCurrentNote() when it's finished.
-
-            if (tailOff == 0.0) // we only need to begin a tail-off if it's not already doing so - the
-                                // stopNote method could be called more than once.
-                tailOff = 1.0;
-        }
-        else
-        {
-            // we're being told to stop playing immediately, so reset everything..
-
-            clearCurrentNote();
-            angleDelta = 0.0;
-        }
+    //     if (allowTailOff)
+    //     {
+    //         // start a tail-off by setting this flag. The render callback will pick up on
+    //         // this and do a fade out, calling clearCurrentNote() when it's finished.
+    //
+    //         if (tailOff == 0.0) // we only need to begin a tail-off if it's not already doing so - the
+    //                             // stopNote method could be called more than once.
+    //             tailOff = 1.0;
+    //     }
+    //     else
+    //     {
+    //         // we're being told to stop playing immediately, so reset everything..
+    //
+    //         clearCurrentNote();
+    //         angleDelta = 0.0;
+    //     }
     }
 
     void pitchWheelMoved (int /*newValue*/) override
@@ -93,51 +98,75 @@ struct SineWaveVoice  : public SynthesiserVoice
 
     void renderNextBlock (AudioSampleBuffer& outputBuffer, int startSample, int numSamples) override
     {
-        if (angleDelta != 0.0)
+      int sample;
+      int channel;
+      float value;
+
+      sample = 0;
+      channel = 0;
+      while (channel < outputBuffer.getNumChannels())
+      {
+
+        while (sample < numSamples)
         {
-            if (tailOff > 0)
-            {
-                while (--numSamples >= 0)
-                {
-                    const float currentSample = (float) (std::sin (currentAngle) * level * tailOff);
-
-                    std::cout << "currentSample = " << currentSample << std::endl;
-
-                    for (int i = outputBuffer.getNumChannels(); --i >= 0;)
-                        outputBuffer.addSample (i, startSample, currentSample);
-
-                    currentAngle += angleDelta;
-                    ++startSample;
-
-                    tailOff *= 0.99;
-
-                    if (tailOff <= 0.005)
-                    {
-                        clearCurrentNote();
-
-                        angleDelta = 0.0;
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                while (--numSamples >= 0)
-                {
-                    const float currentSample = (float) (std::sin (currentAngle) * level);
-
-                    for (int i = outputBuffer.getNumChannels(); --i >= 0;)
-                        outputBuffer.addSample (i, startSample, currentSample);
-
-                    currentAngle += angleDelta;
-                    ++startSample;
-                }
-            }
+          value = _amplitude * sin(2 * double_Pi * _frequency * _time + _phase);
+          outputBuffer.addSample(value, startSample, sample);
+          sample += 1;
         }
+        channel += 1;
+      }
+
+        // if (angleDelta != 0.0)
+        // {
+        //     if (tailOff > 0)
+        //     {
+        //         while (--numSamples >= 0)
+        //         {
+        //             const float currentSample = (float) (std::sin (currentAngle) * level * tailOff);
+        //
+        //             std::cout << "currentSample = " << currentSample << std::endl;
+        //
+        //             for (int i = outputBuffer.getNumChannels(); --i >= 0;)
+        //                 outputBuffer.addSample (i, startSample, currentSample);
+        //
+        //             currentAngle += angleDelta;
+        //             ++startSample;
+        //
+        //             tailOff *= 0.99;
+        //
+        //             if (tailOff <= 0.005)
+        //             {
+        //                 clearCurrentNote();
+        //
+        //                 angleDelta = 0.0;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     else
+        //     {
+        //         while (--numSamples >= 0)
+        //         {
+        //             const float currentSample = (float) (std::sin (currentAngle) * level);
+        //
+        //             for (int i = outputBuffer.getNumChannels(); --i >= 0;)
+        //                 outputBuffer.addSample (i, startSample, currentSample);
+        //
+        //             currentAngle += angleDelta;
+        //             ++startSample;
+        //         }
+        //     }
+        // }
     }
 
 private:
     double currentAngle, angleDelta, level, tailOff;
+
+    float _amplitude;
+    float _frequency;
+    float _phase;
+    float _time;
+    float _deltaTime;
 };
 
 
