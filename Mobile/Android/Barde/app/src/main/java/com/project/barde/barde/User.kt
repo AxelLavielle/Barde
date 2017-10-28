0e9b71b1eddf86a98bedb847b38d0cdaeca26dd8
temichael <temem.michael@outlook.fr>
2017-10-28 19:47:42 +0200

    remove android

--- Mobile/Android/Barde/app/src/main/java/com/project/barde/barde/User.kt
+++
@@ -1,7 +0,0 @@
-package com.project.barde.barde
-
-/**
- * Created by michael on 27/10/2017.
- */
-data class Identity(val FirstName: String, val LasName: String, val UserName: String, val Role: String)
-data class User(val email:String, val password: String, val token:String)
