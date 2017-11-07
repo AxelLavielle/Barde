a6af3a6c9b266055cd28692032fcea0fd2f84ab3
temichael <temem.michael@outlook.fr>
2017-11-07 17:01:05 +0100

    ajout champs date de naissance

--- Mobile/Android/Barde/app/src/main/java/com/project/barde/barde/ui/activities/RegisterActivity.kt
+++ Mobile/Android/Barde/app/src/main/java/com/project/barde/barde/ui/activities/RegisterActivity.kt
@@ -8,6 +8,7 @@ import com.github.kittinunf.result.Result
 import com.google.gson.Gson
 import com.project.barde.barde.R
 import com.project.barde.barde.model.Login
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
 import org.jetbrains.anko.doAsync
 
@@ -20,7 +21,7 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
         toRegister.setOnClickListener{
             doAsync {
-                "http://10.0.2.2:3000/auth/register".httpPost(listOf("email" to register_email.text, "password" to register_password.text, "firstame" to register_firstname.text, "lastName" to register_lastname.text, "userName" to register_username.text)).responseString{ request, response, result ->
+                "http://10.0.2.2:3000/auth/register".httpPost(listOf("email" to register_email.text, "password" to register_password.text, "firstName" to register_firstname.text, "lastName" to register_lastname.text, "userName" to register_username.text, "yearOfBirth" to register_dateofbirthday.year, "monthOfBirth" to register_dateofbirthday.month, "dayOfBirth" to register_dateofbirthday.dayOfMonth)).responseString{ request, response, result ->
                     val register: Login = Gson().fromJson(String(response.data), Login::class.java)
                      Toast.makeText(this@RegisterActivity, register.data.message, Toast.LENGTH_SHORT).show()
                      println(register)
