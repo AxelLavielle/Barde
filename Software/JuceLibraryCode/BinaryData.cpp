7f542b2a0c13b5ad705be46f95e2e192030ad65c
Anthony Vogelweid <anthony.vogelweid@epitech.eu>
2018-01-17 16:01:23 -0500

    Correction compilation Linux

--- Software/JuceLibraryCode/BinaryData.cpp
+++ Software/JuceLibraryCode/BinaryData.cpp
@@ -9,17 +9,17 @@ namespace BinaryData
 
 //================== people.json ==================
 static const unsigned char temp_binary_data_0[] =
-"{\r\n"
-"    \"encoding\" : \"UTF-8\",\r\n"
-"    \"toto\" : \"titi\",\r\n"
-"    \"encodi\" : \"UTF-7\",\r\n"
-"    \"plug-ins\" : [\r\n"
-"        \"python\",\r\n"
-"        \"c++\",\r\n"
-"        \"ruby\"\r\n"
-"        ],\r\n"
-"    \"indent\" : { \"length\" : 3, \"use_space\": true }\r\n"
-"}\r\n";
+"{\n"
+"    \"encoding\" : \"UTF-8\",\n"
+"    \"toto\" : \"titi\",\n"
+"    \"encodi\" : \"UTF-7\",\n"
+"    \"plug-ins\" : [\n"
+"        \"python\",\n"
+"        \"c++\",\n"
+"        \"ruby\"\n"
+"        ],\n"
+"    \"indent\" : { \"length\" : 3, \"use_space\": true }\n"
+"}\n";
 
 const char* people_json = (const char*) temp_binary_data_0;
 
@@ -5796,10 +5796,10 @@ const char* logo_png = (const char*) temp_binary_data_2;
 
 //================== Dark ==================
 static const unsigned char temp_binary_data_3[] =
-"DarkTheme\r\n"
-"#D2005F\r\n"
-"#2c3e50\r\n"
-"#FFFFFF\r\n";
+"DarkTheme\n"
+"#D2005F\n"
+"#2c3e50\n"
+"#FFFFFF\n";
 
 const char* Dark = (const char*) temp_binary_data_3;
 
@@ -5814,10 +5814,10 @@ const char* getNamedResource (const char* resourceNameUTF8, int& numBytes) throw
 
     switch (hash)
     {
-        case 0x441eb498:  numBytes = 215; return people_json;
+        case 0x441eb498:  numBytes = 204; return people_json;
         case 0x7e701ad1:  numBytes = 140671; return back_png;
         case 0x78ded995:  numBytes = 271238; return logo_png;
-        case 0x00206396:  numBytes = 38; return Dark;
+        case 0x00206396:  numBytes = 34; return Dark;
         default: break;
     }
 
--- Software/Source/FileManager.hh
+++ Software/Source/FileManager.hh
@@ -11,21 +11,25 @@
 #ifndef FILEMANAGER_HH_INCLUDED
 #define FILEMANAGER_HH_INCLUDED
 
-#include <Windows.h>
 #include <vector>
+#include <string>
+#include <iostream>
 
 #include <stdio.h>  /* defines FILENAME_MAX */
-// #define WINDOWS  /* uncomment this line to use it for windows.*/ 
+// #define WINDOWS  /* uncomment this line to use it for windows.*/
 #ifdef __linux__
 	#include <unistd.h>
+	#include <sys/types.h>
+	#include <dirent.h>
 #else
+	#include <Windows.h>
 	#include <direct.h>
 #endif
 
 class FileManager
 {
 public:
-	static void getFilesList(std::string filePath, std::string extension, std::vector<std::string> & returnFileName);
+	static void getFilesList(const std::string &filePath, const std::string &extension, std::vector<std::string> & returnFileName);
 	static std::string getFileName(const std::string & filePath);
 	static std::string getCurrentDirectory();
 
