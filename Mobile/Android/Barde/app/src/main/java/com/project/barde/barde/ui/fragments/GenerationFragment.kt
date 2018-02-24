a273e17a96f9e20bc51785b4f883dcf6a3a9d9aa
unknown <temem.michael@outlook.fr>
2018-02-24 17:17:22 +0100

    ajout mock up

--- Mobile/Android/Barde/app/src/main/java/com/project/barde/barde/ui/fragments/GenerationFragment.kt
+++ Mobile/Android/Barde/app/src/main/java/com/project/barde/barde/ui/fragments/GenerationFragment.kt
@@ -2,10 +2,17 @@ package com.project.barde.barde.ui.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.project.barde.barde.R
+import com.project.barde.barde.adapter.PlaylistChoiceAdapter
+import kotlinx.android.synthetic.main.dialog_playlist_choice.*
+import kotlinx.android.synthetic.main.fragment_generation.*
 
 /**
  * Created by michael on 17/02/2018.
@@ -13,5 +20,24 @@ import com.project.barde.barde.R
 class GenerationFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_generation, container, false)
+    }
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        add_to_playlist.setOnClickListener {
+            add_to_playlist()
+        }
+    }
+
+    fun add_to_playlist(){
+        val builder = AlertDialog.Builder(activity)
+        val inflater = layoutInflater
+        val view = inflater.inflate(R.layout.dialog_playlist_choice, null)
+        var playlists = arrayOf<String>("detente", "travail", "etude", "vacance", "relaxation", "travail", "etude", "vacance", "relaxation", "travail", "etude", "vacance", "relaxation", "travail", "etude", "vacance", "relaxation", "travail", "etude", "vacance", "relaxation", "travail", "etude", "vacance", "relaxation")
+        val adapter = PlaylistChoiceAdapter(playlists)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.playlist_reycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = adapter
+        builder.setView(view)
+        val dialog = builder.create()
+        dialog.show()
     }
 }
