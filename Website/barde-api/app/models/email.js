1ccba93ec40a7504caba52699d7317e6a3cdfc21
Alexis Miele <alexis.miele@epitech.eu>
2018-07-30 16:03:02 +0200

    Fix server (NEED npm install)

--- Website/barde-api/app/models/email.js
+++ Website/barde-api/app/models/email.js
@@ -6,7 +6,7 @@
 
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
-var bcrypt   = require('bcrypt');
+var bcrypt   = require('bcrypt-nodejs');
 
 var EmailSchema = new Schema({
     email: {
--- Website/barde-api/app/models/user.js
+++ Website/barde-api/app/models/user.js
@@ -6,7 +6,7 @@
 
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
-var bcrypt   = require('bcrypt');
+var bcrypt   = require('bcrypt-nodejs');
 
 var UserSchema = new Schema({
     email: {
