92da9176ebe368431377c5d764d0527ab63dd7d6
Valérian Polizzi <valerian.polizzi@epitech.eu>
2018-08-08 13:11:11 +0200

    play / stop  sur fichier .mid

--- Website/google-web-app/js/console.js
+++ Website/google-web-app/js/console.js
@@ -14,11 +14,14 @@ class Barde {
 		this.connected = false;
 		this.socket = new TcpClient(DEFAULT_IP, DEFAULT_PORT);
 		this.feed = null;
+		this.player = null;
 		this.socket.addResponseListener(function (res){
 			BardeLog("Server : " + res)
-			console.log(res);
-			BardeLog("To bas64")
 		});
+	}
+
+	loadMidFile(file){
+		this.player = new MidiPlayer('files/bbbbarde.mid', 'control-button-play', true);
 	}
 
 	init() {
@@ -26,6 +29,11 @@ class Barde {
 		document.getElementById('ip').value = this.ip;
 		document.getElementById('port').value = this.port;
 		BardeLog("initializing OK");
+
+		this.loadMidFile("js/bbbbarde.mid");
+
+
+
 	}
 
 	onConnect(connect){
@@ -38,12 +46,11 @@ class Barde {
 	}
 
 	onDisconnect(){
-		BardeLog("Disconnecting ...")
 		document.getElementById('connect').style.display = 'block'
 		document.getElementById('disconnect').style.display = 'none'
 		document.getElementById('control-button-play').disabled = true;
-
 		this.connected = false
+		BardeLog("Disconnected")
 
 	}
 
@@ -56,38 +63,46 @@ class Barde {
 
 	}
 
+
+
 	play(){
 		var buffer = new Uint8Array([0x2, 0, 0, 0, 0x12, 0, 0, 0, 0x0D, 0, 0, 0, 0x0A, 0, 0, 0]);
 		this.send(buffer.buffer);
 		document.getElementById('control-button-play').disabled = true;
 		document.getElementById('control-button-pause').disabled = false;
+		BardeLog("Playing");
 
-		this.socket.addResponseListener(function (res){
-			this.feed = res;
-			BardeLog("Midi " + res);
-		});
+		this.player.play()
 
 	}
 
 	pause(){
 		var buffer = new Uint8Array([0x2, 0, 0, 0, 0x32, 0, 0, 0, 0x0D, 0, 0, 0, 0x0A, 0, 0, 0]);
 		this.send(buffer.buffer);
-		document.getElementById('control-button-play').disabled = false;
+	this.player.stop()
+		BardeLog("Stop");
+
+	document.getElementById('control-button-play').disabled = false;
 		document.getElementById('control-button-pause').disabled = true;
 	}
 
 	disconnect(){
+		this.player.stop();
 		this.socket.disconnect();
+		document.getElementById('control-button-play').disabled = true;
+		document.getElementById('control-button-pause').disabled = true;
+		this.onDisconnect();
 	}
 
 }
 
 
 document.addEventListener("DOMContentLoaded", function() {
-
 	var BardePlayer = new Barde();
 
 	BardePlayer.init();
+
+
 
 	window.onload = function() {
 		document.getElementById('connect').onclick = function fun(elem) {
@@ -100,7 +115,6 @@ document.addEventListener("DOMContentLoaded", function() {
 		document.getElementById('control-button-play').onclick = function fun(elem) {
 			BardePlayer.play();
 		}
-
 
 		document.getElementById('control-button-pause').onclick = function fun(elem) {
 			BardePlayer.pause();
