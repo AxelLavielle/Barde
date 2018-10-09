9dbac8a6ae5058e37122fa32f2a98814601ce43b
Valerian Polizzi <valerian.polizzi@epitech.eu>
2018-10-09 09:36:59 +0200

    mise a jour des liens de telechargement

--- Website/barde-www/src/vues/DownloadBarde/DownloadBarde.js
+++ Website/barde-www/src/vues/DownloadBarde/DownloadBarde.js
@@ -1,13 +1,16 @@
+import links from "../../assets/data/Links"
 
 export default {
 	name: 'DownloadBardeVue',
 	data () {
 		return {
+			"links": links.links
 		}
 	},
 	components : {
 	},
 	mounted: function () {
+		console.log(this.links)
 		$('.tooltipped').tooltip();
 
 	},
