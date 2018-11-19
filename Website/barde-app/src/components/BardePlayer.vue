c033391df44f4989be3f4af61bd5865edf90fc8f
Loïc Weinhard <loic.weinhard@neofacto.com>
2018-11-19 22:05:00 +0100

    MIDI file successfully retrieved from generation server

--- Website/barde-app/src/components/BardePlayer.vue
+++ Website/barde-app/src/components/BardePlayer.vue
@@ -28,126 +28,274 @@
 </template>
 
 <script>
+import VueHowler from "vue-howler";
+import MidiPlayer from "midi-player-js";
+import BardeConsole from "@/components/player/BardeConsole";
+import BardeSettings from "@/components/player/BardeSettings";
+import HolwerPlayer from "@/components/HowlerPlayer";
+import blobToArrayBuffer from "blob-to-arraybuffer";
+import FileSaver from "file-saver";
 
-  import VueHowler from 'vue-howler'
-  import MidiPlayer from 'midi-player-js'
-  import BardeConsole from "@/components/player/BardeConsole"
-  import BardeSettings from "@/components/player/BardeSettings"
-  import HolwerPlayer from "@/components/HowlerPlayer"
-  import blobToArrayBuffer from "blob-to-arraybuffer";
-  import FileSaver from 'file-saver';
+const CMD_PLAYER_PLAY = [
+  0x2,
+  0,
+  0,
+  0,
+  0x12,
+  0,
+  0,
+  0,
+  0x0d,
+  0,
+  0,
+  0,
+  0x0a,
+  0,
+  0,
+  0
+];
+const CMD_PLAYER_PAUSE = [
+  0x2,
+  0,
+  0,
+  0,
+  0x22,
+  0,
+  0,
+  0,
+  0x0d,
+  0,
+  0,
+  0,
+  0x0a,
+  0,
+  0,
+  0
+];
 
-  const CMD_PLAYER_PLAY     = [0x2, 0, 0, 0, 0x12, 0, 0, 0, 0x0D, 0, 0, 0, 0x0A, 0, 0, 0];
-  const CMD_PLAYER_PAUSE    = [0x2, 0, 0, 0, 0x22, 0, 0, 0, 0x0D, 0, 0, 0, 0x0A, 0, 0, 0]
+const BACKSLASHR = [0x0d, 0, 0, 0];
+const BACKSLASHN = [0x0a, 0, 0, 0];
 
-  const BACKSLASHR = [0x0D, 0, 0, 0]
-  const BACKSLASHN = [0x0A, 0, 0, 0]
+const MUSIQUEPARAM = 0x1;
+const STYLE = 0x11;
+const JAZZ = 0x111;
 
-  const MUSIQUEPARAM = 0x1;
-  const STYLE = 0x11;
-  const JAZZ =  0x111;
+const CMD_MUSICPARAM_STYLE_BLUES = [
+  0x1,
+  0,
+  0,
+  0,
+  0x11,
+  0,
+  0,
+  0,
+  0x11,
+  0x2,
+  0,
+  0,
+  0x0d,
+  0,
+  0,
+  0,
+  0x0a,
+  0,
+  0,
+  0
+];
+const CMD_MUSICPARAM_STYLE_RAGGEA = [
+  0x1,
+  0,
+  0,
+  0,
+  0x11,
+  0,
+  0,
+  0,
+  0x11,
+  0x1,
+  0,
+  0,
+  0x0d,
+  0,
+  0,
+  0,
+  0x0a,
+  0,
+  0,
+  0
+];
+const CMD_MUSICPARAM_ADDCHORD_PIANO = [
+  0x1,
+  0,
+  0,
+  0,
+  0x11,
+  0,
+  0,
+  0,
+  0x111,
+  0,
+  0,
+  0,
+  0x0d,
+  0,
+  0,
+  0,
+  0x0a,
+  0,
+  0,
+  0
+];
+const CMD_MUSICPARAM_ADDCHORD_SAXO = [
+  0x1,
+  0,
+  0,
+  0,
+  0x11,
+  0,
+  0,
+  0,
+  0x41,
+  0,
+  0,
+  0,
+  0x0d,
+  0,
+  0,
+  0,
+  0x0a,
+  0,
+  0,
+  0
+];
 
+const CMD_PLAYER_PLAY_RESPONSE = "OKplaying";
 
-  const CMD_MUSICPARAM_STYLE_BLUES = [0x1, 0, 0, 0, 0x11, 0, 0, 0, 0x11, 0x2, 0, 0, 0x0D, 0, 0, 0, 0x0A, 0, 0, 0]
-  const CMD_MUSICPARAM_STYLE_RAGGEA = [0x1, 0, 0, 0, 0x11, 0, 0, 0, 0x11, 0x1, 0, 0, 0x0D, 0, 0, 0, 0x0A, 0, 0, 0]
-  const CMD_MUSICPARAM_ADDCHORD_PIANO = [0x1, 0, 0, 0, 0x11, 0, 0, 0, 0x111, 0, 0, 0, 0x0D, 0, 0, 0, 0x0A, 0, 0, 0]
-  const CMD_MUSICPARAM_ADDCHORD_SAXO = [0x1, 0, 0, 0, 0x11, 0, 0, 0, 0x41, 0, 0, 0, 0x0D, 0, 0, 0, 0x0A, 0, 0, 0]
-
-  const CMD_PLAYER_PLAY_RESPONSE = "OKplaying";
-
-  export default {
-    components:{
-      BardeConsole,
-      BardeSettings,
-      HolwerPlayer
-    },
-    data () {
-      return {
-        websocket : null,
-        playing: false,
-        status: "",
-        file: null,
-        player: null,
-        lastResponse: null,
-        responses: [CMD_PLAYER_PLAY_RESPONSE],
-        actions: [this.onPlayResponse],
-        data: []
-      }
-    },
-    beforeMount(){
-      this.getUser();
-    },
-    mounted () {
-      this.initWebSocket(process.env.NODE_ENV === 'development' ? "ws://163.172.191.206:23344" : "ws://163.172.191.206:23345");
-    },
-    methods: {
-      formatStyle(styleValue){
-        let style = [0x1, 0, 0, 0, 0x11, 0, 0, 0, 0x11, parseInt(styleValue), 0, 0, 0x0D, 0, 0, 0, 0x0A, 0, 0, 0]
-        return style
-      },
-      onStyleChange(value){
-        let style = value.toString().split(":");
-        this.sendCommand(this.formatStyle(style[0]), "USE " + style[1]);
-      },
-      initWebSocket(wsUri){
-        this.websocket = new WebSocket(wsUri, ['binary', 'base64', 'arrayBuffer']);
-        this.websocket.onopen = this.onConnect;
-        this.websocket.onclose = this.onDisconnect;
-        this.websocket.onmessage = this.onMessage;
-        this.websocket.onerror = this.onError;
-
-        },
-      initPlayer(){
-
-        this.player = new MidiPlayer.Player();
-        this.player.on('fileLoaded', function() {
-          console.log("file loaded")
-        });
-        this.player.on('playing', function(currentTick) {
-        });
-        this.player.on('midiEvent', function(event) {
-          // Do something when a MIDI event is fired.
-          // (this is the same as passing a function to MidiPlayer.Player() when instantiating.
-        });
-        this.player.on('endOfFile', function() {
-          console.log("endOfFile")
-        });
-
-      },
-      onConnect(){
-        this.$refs.console.log("Barde is connected")
-        this.status = "Connected";
-
-      } ,
-      blobToArrayBuffer(b){
-        var arrayBuffer;
-        var fileReader = new FileReader();
-        fileReader.onload = function(event) {
-          arrayBuffer = event.target.result;
-        };
-        fileReader.readAsArrayBuffer(blob);
-      },
-      blobToString(b) {
-        var u, x;
-        u = URL.createObjectURL(b);
-        x = new XMLHttpRequest();
-        x.open('GET', u, false); // although sync, you're not fetching over internet
-         x.send();
-        URL.revokeObjectURL(u);
-        return x.responseText;
+export default {
+  components: {
+    BardeConsole,
+    BardeSettings,
+    HolwerPlayer
   },
-      onMessage(e){
-        //let message = this.arrayBufferToString(e.data);
-      let message = (this.blobToString(e.data).replace(/^[a-z\d\-_\s]+$/i, ''));
+  data() {
+    return {
+      websocket: null,
+      playing: false,
+      status: "",
+      file: null,
+      player: null,
+      lastResponse: null,
+      responses: [CMD_PLAYER_PLAY_RESPONSE],
+      actions: [this.onPlayResponse],
+      data: []
+    };
+  },
+  beforeMount() {
+    this.getUser();
+  },
+  mounted() {
+    this.initWebSocket(
+      process.env.NODE_ENV === "development"
+        ? "ws://163.172.191.206:23344"
+        : "ws://163.172.191.206:23345"
+    );
+    this.initPlayer();
+  },
+  methods: {
+    formatStyle(styleValue) {
+      let style = [
+        0x1,
+        0,
+        0,
+        0,
+        0x11,
+        0,
+        0,
+        0,
+        0x11,
+        parseInt(styleValue),
+        0,
+        0,
+        0x0d,
+        0,
+        0,
+        0,
+        0x0a,
+        0,
+        0,
+        0
+      ];
+      return style;
+    },
+    onStyleChange(value) {
+      let style = value.toString().split(":");
+      this.sendCommand(this.formatStyle(style[0]), "USE " + style[1]);
+    },
+    initWebSocket(wsUri) {
+      this.websocket = new WebSocket(wsUri, [
+        "binary",
+        "base64",
+        "arrayBuffer"
+      ]);
+      this.websocket.onopen = this.onConnect;
+      this.websocket.onclose = this.onDisconnect;
+      this.websocket.onmessage = this.onMessage;
+      this.websocket.onerror = this.onError;
+    },
+    initPlayer() {
+      this.player = new MidiPlayer.Player();
+      this.player.on("fileLoaded", function() {
+        console.log("file loaded");
+      });
+      this.player.on("playing", function(currentTick) {});
+      this.player.on("midiEvent", function(event) {
+        // Do something when a MIDI event is fired.
+        // (this is the same as passing a function to MidiPlayer.Player() when instantiating.
+      });
+      this.player.on("endOfFile", function() {
+        console.log("endOfFile");
+      });
+    },
+    onConnect() {
+      this.$refs.console.log("Barde is connected");
+      this.status = "Connected";
+    },
+    blobToArrayBuffer(blob) {
+      var arrayBuffer;
+      var fileReader = new FileReader();
+      fileReader.onload = function(event) {
+        arrayBuffer = event.target.result;
+      };
+      fileReader.readAsArrayBuffer(blob);
+    },
+    blobToString(b) {
+      var u, x;
+      u = URL.createObjectURL(b);
+      x = new XMLHttpRequest();
+      x.open("GET", u, false); // although sync, you're not fetching over internet
+      x.send();
+      URL.revokeObjectURL(u);
+      return x.responseText;
+    },
+    async onMessage(e) {
+      let arrayBuffer = await fetch(URL.createObjectURL(e.data)).then(res =>
+        res.arrayBuffer()
+      );
+      arrayBuffer = arrayBuffer.slice(4);
+      let message = this.blobToString(e.data).replace(/^[a-z\d\-_\s]+$/i, "");
       if (message !== "hello\r\n")
-      this.$refs.console.log(message, "server", message.includes("OK :") ? "ok" : "ko");
+        this.$refs.console.log(
+          message,
+          "server",
+          message.includes("OK :") ? "ok" : "ko"
+        );
 
-      if (this.isMidi(message)){
-        const blob = new Blob( [ e.data ], { type: 'audio/mid' } );
-        FileSaver.saveAs(blob, "./hello world.mid");
-
+      if (this.isMidi(message)) {
+        const player = new MidiPlayer.Player();
+        player.loadArrayBuffer(arrayBuffer);
+        player.play();
       }
-    /*
+      /*
       blobToArrayBuffer(e.data).then(buffer => {
           this.checkAction(buffer.srcElement.result, message, e);
         })
@@ -155,96 +303,92 @@
 
      */
     },
-      debutMessage(){
+    debutMessage() {},
+    arrayContains(needle, arrhaystack) {
+      console.log(
+        needle.substr(3),
+        "in ",
+        arrhaystack,
+        arrhaystack.indexOf(needle.substr(3))
+      );
+      return arrhaystack.indexOf(needle) > -1;
+    },
+    removeNonPrintableChar(str) {
+      return str.replace(/[^ -~]+/g, "");
+    },
+    onDisconnect() {
+      this.status = "Disconnected";
 
+      this.$refs.console.log("disconnected.");
+    },
+    onError(e) {
+      console.log(e);
+      this.$refs.console.log("Service not available");
+    },
+    commandToBuffer(cmd) {
+      var buffer = new Uint8Array(cmd);
+      return buffer;
+    },
+    onPlayResponse() {
+      console.log("play response");
+      this.data = [];
+      this.lastResponse = CMD_PLAYER_PLAY_RESPONSE;
+    },
+    arrayBufferToString(buffer) {
+      return String.fromCharCode.apply(null, new Uint8Array(buffer));
+    },
+    base64ArrayBuffer(arrayBuffer) {
+      var base64 = "",
+        encodings =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+        bytes = new Uint8Array(arrayBuffer),
+        byteLength = bytes.byteLength,
+        byteRemainder = byteLength % 3,
+        mainLength = byteLength - byteRemainder,
+        a,
+        b,
+        c,
+        d,
+        chunk;
 
-      },
-       arrayContains(needle, arrhaystack) {
-        console.log(needle.substr(3), "in ", arrhaystack, arrhaystack.indexOf(needle.substr(3)))
-        return (arrhaystack.indexOf(needle) > -1);
-      },
-      removeNonPrintableChar(str){
-       return str.replace(/[^ -~]+/g, "");
+      for (var i = 0; i < mainLength; i = i + 3) {
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
 
-      },
-      onDisconnect(){
-        this.status = "Disconnected";
+        a = (chunk & 16515072) >> 18;
+        c = (chunk & 4032) >> 6;
+        d = chunk & 63;
 
-        this.$refs.console.log("disconnected.")
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
+      }
 
-      },
-      onError(e){
-        console.log(e);
-        this.$refs.console.log("Service not available")
+      if (byteRemainder == 1) {
+        chunk = bytes[mainLength];
+        a = (chunk & 252) >> 2;
+        b = (chunk & 3) << 4;
+        base64 += encodings[a] + encodings[b] + "==";
+      } else if (byteRemainder == 2) {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+        a = (chunk & 64512) >> 10;
+        b = (chunk & 1008) >> 4;
+        c = (chunk & 15) << 2;
+        base64 += encodings[a] + encodings[b] + encodings[c] + "=";
+      }
+      return base64;
+    },
+    isMidi(str) {
+      if (str.indexOf("MTh") !== -1) return true;
+      return false;
+    },
 
-      },
-      commandToBuffer(cmd){
-        var buffer = new Uint8Array(cmd);
-        return buffer
-      },
-      onPlayResponse(){
-        console.log("play response")
-        this.data = [];
-        this.lastResponse = CMD_PLAYER_PLAY_RESPONSE;
-      },
-      arrayBufferToString(buffer) {
-        return String.fromCharCode.apply(null, new Uint8Array(buffer));
-      },
-      base64ArrayBuffer(arrayBuffer) {
-        var
-          base64 = "",
-          encodings = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
-          bytes = new Uint8Array(arrayBuffer),
-          byteLength = bytes.byteLength,
-          byteRemainder = byteLength % 3,
-          mainLength = byteLength - byteRemainder,
-          a,
-          b,
-          c,
-          d,
-          chunk;
-
-        for (var i = 0; i < mainLength; i = i + 3) {
-          chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
-
-          a = (chunk & 16515072) >> 18;
-          c = (chunk & 4032) >> 6;
-          d = chunk & 63;
-
-          base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
-        }
-
-        if (byteRemainder == 1) {
-          chunk = bytes[mainLength];
-          a = (chunk & 252) >> 2;
-          b = (chunk & 3) << 4;
-          base64 += encodings[a] + encodings[b] + "==";
-        }
-        else if (byteRemainder == 2) {
-          chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
-          a = (chunk & 64512) >> 10;
-          b = (chunk & 1008) >> 4;
-          c = (chunk & 15) << 2;
-          base64 += encodings[a] + encodings[b] + encodings[c] + "=";
-    }
-    return base64;
-  },
-      isMidi(str){
-        if (str.indexOf("MTh") !== -1)
-          return true;
-        return false
-      },
-
-      createMidi(blob){
-        console.log(blob)
-        FileSaver.saveAs(blob, "./hello world.mid");
-
-      },
-      checkAction(buffer, message, blob){
-        if (message.indexOf("MTh") >= 0){
-          this.createMidi(blob.data);
-        }
-        /*
+    createMidi(blob) {
+      console.log(blob);
+      // FileSaver.saveAs(blob, "./hello world.mid");
+    },
+    checkAction(buffer, message, blob) {
+      if (message.indexOf("MTh") >= 0) {
+        //this.createMidi(blob.data);
+      }
+      /*
         var index = this.strInArray(message, this.responses);
         console.log(message, " index: ", index)
         if (index !== -1){
@@ -261,78 +405,75 @@
           }
         }
         */
-      },
-      saveData (data, filename){
-        var a = document.createElement("a");
-        document.body.appendChild(a);
-        a.style = "display: none";
-        return function (data, fileName) {
-          var json = JSON.stringify(data),
-            blob = new Blob([json], {type: "octet/stream"}),
-            url = window.URL.createObjectURL(blob);
-          a.href = url;
-          a.download = fileName;
-          a.click();
-          window.URL.revokeObjectURL(url);
-        };
-      },
-      createFile(buffer){
-        consike
-        /*
+    },
+    saveData(data, filename) {
+      var a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+      return function(data, fileName) {
+        var json = JSON.stringify(data),
+          blob = new Blob([json], { type: "octet/stream" }),
+          url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+      };
+    },
+    createFile(buffer) {
+      consike;
+      /*
            BUFFER -> BARDE.MID / .MIDI
       */
-
-      },
-      strInArray(str, array){
-        for (var i = 0; i < array.length; i++){
-          if (array[i].indexOf(str) !== -1){
-            return i
-          }
-        }
-        return -1
-      },
-      inResponseArray(response){
-        return this.responses.includes(response)
-      },
-      sendCommand(cmd, name){
-        this.$refs.console.log(name, "me")
-        this.websocket.send(this.commandToBuffer(cmd))
-      },
-      addSlashes(str){
-        str = str.replace(/\\'/g, '\'');
-        str = str.replace(/\\"/g, '"');
-        str = str.replace(/\\0/g, '\0');
-        str = str.replace(/\\\\/g, '\\');
-        return str
-        },
-      CommandPlay(){
-        this.playing = true;
-        this.sendCommand(CMD_PLAYER_PLAY, "PLAY");
-        },
-      CommandPause(){
-        this.$refs.console.log("PAUSE", "me")
-        this.playing = false;
-        this.sendCommand(CMD_PLAYER_PAUSE);
-
-      },
-      epurString(str){
-        str = str.substr(12);
-        str = str.substring(0, str.length - 2)
-        return str
-      },
-      playFile(){
-        this.player.loadArrayBuffer(this.file.buffer)
-      },
-      getUser(){
-        var jwtDecode = require('jwt-decode');
-        this.user = jwtDecode(this.$auth.token('default'));
-      },
-      add() {
-        // Emit the server side
-        this.$socket.emit("add", { a: 5, b: 3 });
-      }
     },
-
+    strInArray(str, array) {
+      for (var i = 0; i < array.length; i++) {
+        if (array[i].indexOf(str) !== -1) {
+          return i;
+        }
+      }
+      return -1;
+    },
+    inResponseArray(response) {
+      return this.responses.includes(response);
+    },
+    sendCommand(cmd, name) {
+      this.$refs.console.log(name, "me");
+      this.websocket.send(this.commandToBuffer(cmd));
+    },
+    addSlashes(str) {
+      str = str.replace(/\\'/g, "'");
+      str = str.replace(/\\"/g, '"');
+      str = str.replace(/\\0/g, "\0");
+      str = str.replace(/\\\\/g, "\\");
+      return str;
+    },
+    CommandPlay() {
+      this.playing = true;
+      this.sendCommand(CMD_PLAYER_PLAY, "PLAY");
+    },
+    CommandPause() {
+      this.$refs.console.log("PAUSE", "me");
+      this.playing = false;
+      this.sendCommand(CMD_PLAYER_PAUSE);
+    },
+    epurString(str) {
+      str = str.substr(12);
+      str = str.substring(0, str.length - 2);
+      return str;
+    },
+    playFile() {
+      this.player.loadArrayBuffer(this.file.buffer);
+    },
+    getUser() {
+      var jwtDecode = require("jwt-decode");
+      this.user = jwtDecode(this.$auth.token("default"));
+    },
+    add() {
+      // Emit the server side
+      this.$socket.emit("add", { a: 5, b: 3 });
+    }
   }
+};
 </script>
 
